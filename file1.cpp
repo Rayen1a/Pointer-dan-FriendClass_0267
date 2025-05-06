d7d7b652ea1541639f001a387249e3028c3bc338
Rayen1a <raihan01426@gmail.com>
2025-05-06 10:57:19 +0700

    Int main

--- file1.cpp
+++ file1.cpp
@@ -38,3 +38,15 @@ class PersegiPanjang
             return 3.14 * r * r;
         } 
     };
+
+    int main()
+    {
+        PersegiPanjang objekPP; // membuat objek
+        Lingkaran ol;
+
+        cout << "Masukkan Panjang : ";
+        cin >> objekPP.panjang;
+        cout << "Masukkan Lebar : ";
+        cin >> objekPP.lebar;
+        cout << "Luas Persegi Panjang : " << objekPP.luasPp(objekPP.panjang, objekPP.lebar) << endl; 
+    };
